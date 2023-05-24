0a5cebcbdc1ea4220d766b4bb8171edaf883ec19
Moaz Samy <moazsamy333@gmail.com>
2023-05-24 19:03:18 +0200

    Create views for menu items, booking and user models

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,5 +1,22 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.contrib.auth.models import User
+from rest_framework import generics, viewsets, permissions
 
-def index(request):
-    return render(request, 'index.html', {})
+from .serializers import menuSerializer, bookingSerializer, UserSerializer
+from .models import Menu, Booking
+
+class MenuItemsView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = menuSerializer
+
+class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = menuSerializer
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = bookingSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
