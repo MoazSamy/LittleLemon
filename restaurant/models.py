bfb6748b4c598ac8232962b1cc5caf249a32dd5c
Moaz Samy <moazsamy333@gmail.com>
2023-05-24 18:05:52 +0200

    Adding Booking and Menu tables and registering them in Admin site

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Booking(models.Model):
+    name = models.CharField(max_length=255)
+    no_of_guests = models.SmallIntegerField()
+    bookingDate = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Menu(models.Model):
+    Title = models.CharField(max_length=255)
+    Price = models.DecimalField(max_digits=10, decimal_places=2)
+    Inventory = models.PositiveSmallIntegerField()
+    
+    def __str__(self):
+        return self.Title
