5f6c9bda071283a90de77111d41e25c2d5f08cf9
Moaz Samy <moazsamy333@gmail.com>
2023-05-25 14:44:45 +0200

    Add app_name variable to the urls

--- LittleLemonAPI/urls.py
+++ LittleLemonAPI/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 from rest_framework.authtoken.views import obtain_auth_token
 from . import views
 
+app_name = "LittleLemonAPI"
 urlpatterns = [
-    path('menu-items/', views.MenuItemsView.as_view()),
+    path('menu-items/', views.MenuItemsView.as_view(), name="menu-items"),
     path('menu-items/<int:pk>', views.SingleMenuItemView.as_view()),
     path('message/', views.msg),
     path('api-token-auth/', obtain_auth_token),
