64fae5edb9fda7c451fdb930f2cd8d5bff8860b6
Moaz Samy <moazsamy333@gmail.com>
2023-05-25 14:45:07 +0200

    Add index url and index view

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from rest_framework.authtoken.views import obtain_auth_token
+from .views import index
 
 urlpatterns = [
+    path('', index),
     path('api-token-auth/', obtain_auth_token)
 ]
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,10 +1,12 @@
+from django.shortcuts import render
 from django.contrib.auth.models import User
 from rest_framework import viewsets, permissions
 
 from .serializers import bookingSerializer, UserSerializer
 from .models import Booking
 
-
+def index(request):
+    return render(request, "index.html", {"message":"This is the static content."})
 class BookingViewSet(viewsets.ModelViewSet):
     queryset = Booking.objects.all()
     serializer_class = bookingSerializer
